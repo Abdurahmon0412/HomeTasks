46088a4f94a7069496d9befabcd28cc3faf0ea70
Abdurahmon Sadriddinov <abdurahmonsadriddinov0412@gmail.com>
2023-09-13 10:05:43 +0500

    Hometask N36 is done

---
+++ N36_Task1_Record/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N36_Task1_Record.Models
+{
+    public record Person(string FirstName, string LastName);
+
+    public record UserPerson(string FirstName, string LastName, string EmailAddress, string Password)
+        : Person(FirstName, LastName);
+
+    public record Employee(string FirstName, string LastName, string Graduate);
+
+    public record UserEmployee(string FirstName, string LastName,string Graduate, decimal Salary)
+        :Employee(FirstName,LastName,Graduate);
+
+
+    public record Manager(string FirstName, string LastName, string Graduate,decimal Salary,string Password)
+        :Employee(FirstName, LastName,Graduate);
+};
--- N36_Task1_Record/Program.cs
+++ N36_Task1_Record/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using N36_Task1_Record.Models;
+
+Console.WriteLine("hello");
+var user  = new UserPerson("Alisher" , "Gani", "nimai@gmail.com","123456789");
+var employee = new UserEmployee("AbduQodir","Eshmat","Adminka", 120000);
+var manager = new Manager("Firdavs", "Asadov", "Katta Manager", 1000, "djasds123");
