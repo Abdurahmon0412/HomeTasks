b15de44c2629686c64a14f27fa39e1e8c03c1403
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-17 09:50:12 +0500

    Add N55 home task

---
+++ WorkingWithFiles/FilesManagement/FilesManagementService.cs
@@ -0,0 +1,33 @@
+﻿namespace WorkingWithFiles.FilesManagementService;
+
+public class FilesManagementService
+{
+    public void FilesAndFoldersCount(string folderPath)
+    {
+        var directory = new DirectoryInfo(folderPath);
+        var files = directory.GetFiles();
+        var directories = directory.GetDirectories();
+
+        Console.WriteLine(directories.Count());
+        Console.WriteLine(files.Count());
+
+    }
+
+    public void AllFilesLenght(string folderPath)
+    {
+        var directory = new DirectoryInfo(folderPath);
+        var files = directory.GetFiles();
+        var directories = directory.GetDirectories();
+
+
+        var filesLength = 0;
+        var theMostFileLenght = 0;
+        foreach (var file in files)
+        {
+            filesLength += (int)file.Length;
+            if(theMostFileLenght < file.Length)
+                theMostFileLenght = (int)file.Length;
+        }
+        Console.WriteLine($"all files lenght {filesLength} the most length {theMostFileLenght}");
+    }
+}
---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,13 @@
+﻿//- fayllar umumiy hajmi ( size ) va eng katta fayl qaysi ekanligini qaytaradigan service
+//va methodlar yozing ( nomlarini o'zingiz o'ylab ko'ring ) 
+//- tepada aytilgan ma'lumotlarni ekranga chiqaring
+
+using WorkingWithFiles.FilesManagementService;
+
+var folderPath = @"D:\Projects\HighLavelHomeTask\HomeTasks\WorkingWithFiles\bin\Debug\net7.0\Folders";
+//var targetFileName = @"D:\Projects\HighLavelHomeTask\HomeTasks\WorkingWithFiles\bin\Debug\net7.0";
+
+
+var service = new FilesManagementService();
+service.FilesAndFoldersCount(folderPath);
+service.AllFilesLenght(folderPath);
