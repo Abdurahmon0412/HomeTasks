387b130e4be0c1d5a17cd4ed6e0374f758028d85
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-25 15:45:32 +0500

    end task

--- FileExplorerAPI_Task/Controllers/FilesController .cs
+++ FileExplorerAPI_Task/Controllers/FilesController .cs
@@ -1,4 +1,6 @@
-﻿using FileExplorerApplication_Task.FileStorage.Models.Filtering;
+﻿using FileExplorerAPI_Task.Models.Dtos;
+using FileExplorerApplication_Task.FileStorage.Models.Filtering;
+using FileExplorerApplication_Task.FileStorage.Models.Storage;
 using FileExplorerApplication_Task.FileStorage.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,5 +21,43 @@ public class FilesController : ControllerBase
         return result != null ?
             Ok(result)
             : BadRequest();
+    }
+
+    [HttpGet]
+    public ValueTask<IActionResult> GetFiles([FromQuery] FilterModel filterModel, [FromServices] IWebHostEnvironment environment)
+    {
+        var allFiles = new List<StorageFile>();
+
+        var files = GetAllFiles(environment.WebRootPath, allFiles)
+            .Skip((filterModel.PageToken - 1) * filterModel.PageSize)
+            .Take(filterModel.PageSize);
+
+        return new ValueTask<IActionResult>(Ok(files));
+
+    }
+
+    private IList<StorageFile> GetAllFiles(string path, List<StorageFile> allFiles)
+    {
+        var files = new DirectoryInfo(path).GetFiles().ToList();
+
+        var directories = Directory.GetDirectories(path);
+
+        allFiles.AddRange(files.Select(fileInfo => new StorageFile
+        {
+            Name = fileInfo.Name,
+
+            Path = fileInfo.FullName,
+
+            DirectoryPath = fileInfo.DirectoryName!,
+
+            Extension = fileInfo.Extension,
+
+            Size = fileInfo.Length,
+        }));
+
+        foreach (var directory in directories)
+            GetAllFiles(directory, allFiles);
+
+        return allFiles;
     }
 }
---
+++ FileExplorerAPI_Task/Models/Dtos/FilterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FileExplorerAPI_Task.Models.Dtos;
+
+public class FilterModel
+{
+    public int PageSize { get; set; } = 10;
+
+    public int PageToken { get; set; } = 1;
+
+    public bool IncludeDirectories { get; set; } = true;
+
+    public bool IncludeFiles { get; set; } = true;
+
+    public string? SearchKeyword { get; set; }
+}
