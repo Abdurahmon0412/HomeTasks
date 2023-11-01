bec5944ce10e2c069b5eaff3ea29b8c35dbb5085
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-01 11:44:29 +0500

    Add Amenitycontroller

--- Library.API/Controllers/AuthorsController.cs
+++ Library.API/Controllers/AuthorsController.cs
@@ -8,16 +8,27 @@ namespace Library.API.Controllers;
 [Route("api/[controller]")]
 public class AuthorsController : ControllerBase
 {
-    private readonly IEntityBaseService<Book> _bookService;
     private readonly IEntityBaseService<Author> _authorService;
 
-    public AuthorsController(IEntityBaseService<Book> baseService,
-        IEntityBaseService<Author> authorService)
-    {
-        _authorService = authorService;
-        _bookService = baseService;
-    }
+    public AuthorsController(IEntityBaseService<Author> authorService) => _authorService = authorService;
 
-    //[HttpPost("authors")]
+    [HttpPost("authors")]
+    public async ValueTask<IActionResult> AddAuthors([FromBody] Author author) 
+        => Ok(await _authorService.CreateAsync(author));
 
+    [HttpGet("authors")]
+    public IActionResult GetAuthors()
+        => Ok(_authorService.Get(author => true));
+
+    [HttpGet("authors/{authorsId:guid}")]
+    public async ValueTask<IActionResult> GetAuthorsById(Guid authorsId)
+        => Ok(await _authorService.GetByIdAsync(authorsId));
+
+    [HttpPut("amenities/update")]
+    public async ValueTask<IActionResult> UpdateAuthors([FromBody] Author author)
+        => Ok(await _authorService.UpdateAsync(author));
+
+    [HttpDelete("authors/delete")]
+    public async ValueTask<IActionResult> DeleteAuthors([FromBody] Author author)
+        => Ok(await _authorService.DeleteAsync(author));
 }
